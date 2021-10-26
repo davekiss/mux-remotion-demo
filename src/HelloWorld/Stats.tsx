304cc0752e8f04cff3da87380f21317f85972377
Dave Kiss <dave@davekiss.com>
2021-10-26 11:47:01 -0400

    Make request with timeframes

--- src/HelloWorld/Stats.tsx
+++ src/HelloWorld/Stats.tsx
@@ -1,55 +1,58 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { interpolate, useCurrentFrame, continueRender, delayRender } from 'remotion';
 import { COLOR_1 } from './config';
+import useTimeframes from "../hooks/useTimeframes"
+
+type OverallViewsResponse = {
+  data: { total_watch_time: number; total_views: number; }
+  timeframe: number[];
+}
 
 export const Stats: React.FC = () => {
   const frame = useCurrentFrame();
+  const opacity = interpolate(frame, [0, 30], [0, 1]);
+  const timeframes = useTimeframes();
 
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<OverallViewsResponse | null>(null)
   const [handle] = useState(() => delayRender())
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     const headers = new Headers();
     const username = process.env.MUX_PUBLIC_KEY;
     const password = process.env.MUX_SECRET_KEY;
     headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
-    const response = await fetch('https://api.mux.com/data/v1/metrics/views/insights', { headers })
-    const json = await response.json()
+    const response = await fetch(`https://api.mux.com/data/v1/metrics/views/overall?timeframe[]=${timeframes.pastMonth[0]}&timeframe[]=${timeframes.pastMonth[1]}`, { headers })
+    const json = await response.json() as OverallViewsResponse
     setData(json)
-
     continueRender(handle)
-  }
+  }, [handle])
+
 
   useEffect(() => {
     fetchData()
-  }, []);
+  }, [fetchData]);
 
   return (
     <div
       style={{
         fontFamily: 'Helvetica, Arial',
-        fontSize: 40,
+        fontSize: 90,
         textAlign: 'center',
         position: 'absolute',
         bottom: 140,
         width: '100%',
+        color: COLOR_1,
+        opacity
       }}
     >
 
-      <div>
-        {data ? (
-          <div>This video has data from an API! {JSON.stringify(data)}</div>
-        ) : null}
-      </div>
+      {data && (
+        <>
+          <div>{new Intl.NumberFormat().format(data.data.total_views)} total views</div>
+          <div>From {new Date(data.timeframe[0] * 1000).toISOString()} to {new Date(data.timeframe[1] * 1000).toISOString()}</div>
+        </>
+      )}
     </div>
   );
 };
-
-
-
-
-
-export const MyVideo = () => {
-
-}
