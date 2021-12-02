e4f15730ca83bd103927c9bf1df4b7cc5f554581
Dave Kiss <dave@davekiss.com>
2021-12-02 10:08:23 -0500

    Implementing svg piechart animation

--- src/Elements/Browsers.tsx
+++ src/Elements/Browsers.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { interpolate, useCurrentFrame } from 'remotion';
+import { interpolate, useCurrentFrame, Img, spring, useVideoConfig } from 'remotion';
 import { format } from 'date-fns';
 import Chrome from '../../node_modules/browser-logos/src/chrome/chrome_256x256.png';
 import Firefox from '../../node_modules/browser-logos/src/firefox/firefox_256x256.png';
@@ -9,13 +9,45 @@ import Edge from '../../node_modules/browser-logos/src/edge/edge_256x256.png';
 import Opera from '../../node_modules/browser-logos/src/opera/opera_256x256.png';
 import AndroidWebview from '../../node_modules/browser-logos/src/android-webview/android-webview_256x256.png';
 import SamsungInternet from '../../node_modules/browser-logos/src/samsung-internet/samsung-internet_256x256.png';
-import { Img } from "remotion";
 
 import data from "../data/unique_viewers_by_browser.json"
+import { formatNumber } from '../utils';
 
 const DateRange = ({ children }: { children: React.ReactNode }) => (
   <div className="text-gray-600 text-2xl tracking-wide">{children}</div>
 )
+
+// Passing index allows us to cascade the measurement animation
+const Measure = ({ index, value, percentage }: { index: number, value: number, percentage: number }) => {
+  const frame = useCurrentFrame();
+  const videoConfig = useVideoConfig();
+
+  const m = spring({
+    frame: frame - 10 - (index * 3), // delay the starting frame of the animation
+    from: 0,
+    to: percentage,
+    fps: videoConfig.fps
+  });
+
+  return (
+    <div className="relative w-80 h-80 items-center flex justify-center">
+      <svg className="w-80 h-80 absolute" viewBox="0 0 20 20">
+        <circle r="10" cx="10" cy="10" fill="gray" />
+        <circle
+          r="5" cx="10" cy="10"
+          fill="transparent"
+          stroke="white"
+          strokeWidth="10"
+          strokeDasharray={`calc(${m} * 31.42 / 100) 31.42`}
+          transform="rotate(-90) translate(-20)" />
+      </svg>
+      <div className="z-10 flex flex-col items-center justify-center">
+        <span className="font-sans font-semibold" style={{ fontSize: "80px" }}>{percentage.toFixed(1)}%</span>
+        <span className="font-sans font-normal" style={{ fontSize: "36px" }}>{formatNumber(value)}</span>
+      </div>
+    </div>
+  )
+}
 
 const LOGO_LOOKUP: Record<string, string> = {
   Chrome,
@@ -32,6 +64,7 @@ const LOGO_LOOKUP: Record<string, string> = {
 export const Browsers: React.FC = () => {
   const frame = useCurrentFrame();
   const opacity = interpolate(frame, [0, 30], [0, 1]);
+  const totalDatasetViewers = data[0].data.map(d => d.value).reduce((previousValue, currentValue) => previousValue + currentValue);
 
   return (
     <div
@@ -44,16 +77,15 @@ export const Browsers: React.FC = () => {
       }}
       className="left-0"
     >
-
       <div className="grid grid-cols-5">
-        {data[0].data.map(row => {
+        {data[0].data.map((row, i) => {
           const icon = LOGO_LOOKUP[row.field];
           if (!icon) return;
           return (
-            <div key={row.field}>
-              <Img src={icon} className="mb-4" />
-              <p className="text-2xl">{row.field}</p>
-              <p className="text-2xl">{row.value} unique viewers</p>
+            <div key={row.field} className="flex flex-col items-center">
+              <Measure index={i} value={row.value} percentage={(row.value / totalDatasetViewers) * 100} />
+              {/* <Img src={icon} className="mb-4" /> */}
+              <p className="text-2xl font-mono">{row.field}</p>
             </div>
           )
         })}
