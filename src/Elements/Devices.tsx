f853b1a9a0ce9555b6a46f149d2cead72c8287f6
Dave Kiss <dave@davekiss.com>
2021-12-03 14:47:02 -0500

    Cascade rows

--- src/Elements/Devices.tsx
+++ src/Elements/Devices.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { useCurrentFrame, useVideoConfig, spring } from 'remotion';
+import { useCurrentFrame, useVideoConfig, spring, interpolate } from 'remotion';
 import { gradients } from './config';
 import data from "../data/views_by_device.json";
 import Layout from "../components/Layout";
 
-const Stat = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex items-start border-t border-gray-900 p-4 relative">{children}</div>
-)
+const Stat = ({ index, children }: { index: number, children: React.ReactNode }) => {
+  const frame = useCurrentFrame();
+  const videoConfig = useVideoConfig();
+
+  const offset = spring({
+    frame: frame - 10 - (index * 8), // delay the starting frame of the animation
+    from: -100,
+    to: 0,
+    fps: videoConfig.fps,
+    config: {
+      damping: 60
+    }
+  });
+
+  const opacity = interpolate(frame, [10 + (index * 8), 20 + (index * 8)], [0, 1]);
+
+  return (
+    <div className="flex items-start border-t border-gray-900 p-4 relative" style={{ transform: `translateY(${offset}px)`, opacity }}>{children}</div>
+  )
+}
 
 // Passing index allows us to cascade the measurement animation
 const Measure = ({ index, value }: { index: number, value: number }) => {
@@ -43,7 +60,7 @@ export const Devices: React.FC = () => {
     <Layout background={gradients.yellowGreen} title="Views by device" timeframe={data[0].timeframe} >
       <div className="grid grid-rows-5">
         {data[0].data.map((device, i) => (
-          <Stat key={device.value}>
+          <Stat key={device.value} index={i}>
             <Measure index={i} value={(device.views / totalDatasetViews) * 100} />
             <Value>{new Intl.NumberFormat().format(device.views)}</Value>
             <Label>{device.field}</Label>
