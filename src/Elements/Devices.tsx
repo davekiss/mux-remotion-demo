e78f6d6bb7eb3ee00ae15bc6c55e932438ef320a
Dave Kiss <dave@davekiss.com>
2021-12-15 18:14:29 -0500

    Add device icons and intro animations

--- src/Elements/Devices.tsx
+++ src/Elements/Devices.tsx
@@ -5,6 +5,18 @@ import data from "../data/views_by_device.json";
 import Layout from "../components/Layout";
 import Measure from "../components/Measure";
 
+import Tablet from '../components/icons/Tablet';
+import Desktop from '../components/icons/Desktop';
+import Tv from '../components/icons/Tv';
+import Phone from '../components/icons/Mobile';
+
+const DEVICE_LOOKUP = {
+  phone: Phone,
+  tv: Tv,
+  desktop: Desktop,
+  tablet: Tablet
+}
+
 const Stat = ({ index, children }: { index: number, children: React.ReactNode }) => {
   const frame = useCurrentFrame();
   const videoConfig = useVideoConfig();
@@ -22,7 +34,7 @@ const Stat = ({ index, children }: { index: number, children: React.ReactNode })
   const opacity = interpolate(frame, [10 + (index * 8), 20 + (index * 8)], [0, 1]);
 
   return (
-    <div className="flex items-start border-t-2 border-mux-green-darker p-4 relative" style={{ transform: `translateY(${offset}px)`, opacity }}>{children}</div>
+    <div className="flex items-center border-t-2 border-mux-green-darker p-4 relative" style={{ transform: `translateY(${offset}px)`, opacity }}>{children}</div>
   )
 }
 
@@ -40,13 +52,20 @@ export const Devices: React.FC = () => {
   return (
     <Layout background={gradients.yellowGreen} title="Views by device" timeframe={data[0].timeframe} >
       <div className="grid grid-rows-5">
-        {data[0].data.map((device, i) => (
-          <Stat key={device.value} index={i}>
-            <Measure index={i} value={(device.views / totalDatasetViews) * 100} />
-            <Value>{new Intl.NumberFormat().format(device.views)}</Value>
-            <Label>{device.field}</Label>
-          </Stat>
-        ))}
+        {data[0].data.map((device, i) => {
+          const Icon = DEVICE_LOOKUP[device.field]
+
+          return (
+            <Stat key={device.value} index={i}>
+              <div className="z-10 w-20 mr-20">
+                <Icon />
+              </div>
+              <Measure index={i} value={(device.views / totalDatasetViews) * 100} />
+              <Value>{new Intl.NumberFormat().format(device.views)}</Value>
+              <Label>{device.field}</Label>
+            </Stat>
+          )
+        })}
       </div>
     </Layout>
   );
--- src/Elements/Intro.tsx
+++ src/Elements/Intro.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Layout from "../components/Layout";
 import { format } from 'date-fns';
-
+import { interpolate, useCurrentFrame, Easing } from "remotion";
 import data from "../data/views_by_title.json";
 
 export const Intro: React.FC = () => {
   const { timeframe } = data[0];
+  const frame = useCurrentFrame();
+  const titleOpacity = interpolate(frame, [5, 25], [0, 1]);
+  const subtitleOpacity = interpolate(frame, [30, 50], [0, 1]);
+  const titleY = interpolate(frame, [5, 25], [50, 0], { extrapolateRight: "clamp", easing: Easing.out(Easing.ease) });
+  const subtitleY = interpolate(frame, [30, 50], [50, 0], { extrapolateRight: "clamp", easing: Easing.out(Easing.ease) });
+
   return (
     <Layout background="white">
       <div className="border-b border-mux-gray my-20">
-        <h1 className="text-mux-black leading-none" style={{ fontSize: "100px" }}>Video stats overview</h1>
-        <h2 className="text-mux-gray mb-48" style={{ fontSize: "100px" }}>
+        <h1 className="text-mux-black leading-none" style={{ fontSize: "100px", opacity: titleOpacity, transform: `translateY(${titleY}px)` }}>Video stats overview</h1>
+        <h2 className="text-mux-gray mb-48" style={{ fontSize: "100px", opacity: subtitleOpacity, transform: `translateY(${subtitleY}px)` }}>
           {format(new Date(timeframe[0] * 1000), 'MMM. dd')} – {format(new Date(timeframe[1] * 1000), 'MMM. dd, yyyy')}
         </h2>
       </div>
