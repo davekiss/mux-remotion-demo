03e1f52dc58ceba258ab072b30320a6544154dc1
Dave Kiss <dave@davekiss.com>
2021-12-15 16:45:07 -0500

    Custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,19 @@ module.exports = {
         'mux-gray': '#8e8e8e',
         'mux-red': '#DF2868',
         'mux-pink': '#ffe9f5',
+        'mux-pink-darker': '#fa2186',
+        'mux-pink-darkest': '#d4146c',
         'mux-green': '#eaf9e4',
+        'mux-green-darker': '#1dbb9e',
+        'mux-green-darkest': '#057460',
         'mux-blue': '#acdcff',
+        'mux-blue-darker': '#1a96fd',
+        'mux-blue-darkest': '#0e7ad6',
         'mux-lavendar': '#e3b5ff',
-        'mux-yellow': '#fff7dc'
+        'mux-purple': '#8b1dd3',
+        'mux-yellow': '#fff7dc',
+        'mux-yellow-darker': '#FED32F',
+        'mux-yellow-darkest': "#E99001"
       }
     }
   }
