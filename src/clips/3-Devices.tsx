febec2cf41faecd84e56572d8cccc7f148a831a5
Dave Kiss <dave@davekiss.com>
2022-01-03 14:51:48 -0500

    Rename for clarity

--- src/clips/3-Devices.tsx
+++ src/clips/3-Devices.tsx
@@ -52,7 +52,7 @@ const Label = ({ children }: { children: React.ReactNode }) => (
 )
 
 export const Devices: React.FC = () => {
-  const maxDatasetViews = data[0].data.sort((a, b) => b.views - a.views)[0].views;
+  const leadingDeviceViews = data[0].data.sort((a, b) => b.views - a.views)[0].views;
 
   const frame = useCurrentFrame();
   const { fps } = useVideoConfig();
@@ -78,7 +78,7 @@ export const Devices: React.FC = () => {
               <div className="z-10 w-28 pl-6 mr-20">
                 <Icon />
               </div>
-              <Measure index={i} value={(device.views / maxDatasetViews) * 100} />
+              <Measure index={i} value={(device.views / leadingDeviceViews) * 100} />
               <Value>{formatNumber(totalViews)}</Value>
               <Label>{device.field === "tv" ? "TV" : device.field}</Label>
               <div className="flex justify-end -mt-4" style={{ width: "550px" }}>
